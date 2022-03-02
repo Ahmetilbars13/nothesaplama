13580d3e78940f2b001923b3217b1aef0c4ecfb4
Ahmetilbars13 <100728851+Ahmetilbars13@users.noreply.github.com>
2022-03-02 15:33:20 +0300

    Add files via upload

---
+++ ahmet/script.js
@@ -0,0 +1,24 @@
+document.getElementById("hesapla").addEventListener("click" , notHesaplama)
+function notHesaplama(){
+    let yazili1=document.getElementById("y1").value;
+    let yazili2=document.getElementById("y2").value;
+    let performans=document.getElementById("p1").value;
+    let ortalama=(Number(yazili1) +  Number(yazili2) + Number(performans) )/3;
+    let durum="heaplama";
+    if (ortalama<=50){
+        durum="KALDI";
+        
+    }
+    else{
+        durum="GEÇTİ";
+        
+    }
+    document.getElementById("sonuc").innerHTML = durum + " " + ortalama.toFixed(1);
+}
+
+   
+            
+        
+    
+    
+
